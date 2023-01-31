218e742bb25b23674ac05de76b24d746160caff3
Lucía Mariel Alonso <89427680+Azuquitaa@users.noreply.github.com>
2023-01-30 22:59:01 -0300

    Ultimos cambios, se agregó js
    Se agregaron botones dinámicos

---
+++ js/funciones.js
@@ -0,0 +1,27 @@
+
+
+document.getElementById("a").onclick = function () {
+    
+    document.getElementById("a").innerHTML="Inglés <br> Intermedio";
+}
+document.getElementById("b").onclick = function () {
+    document.getElementById("b").innerHTML="Trabajo <br> Grupal"
+}
+document.getElementById("c").onclick = function () {
+    document.getElementById("c").innerHTML="Aprendizaje <br> Rápido y capaz"
+}
+document.getElementById("d").onclick = function () {
+    document.getElementById("d").innerHTML="Cambios <br> Adaptable"
+}
+
+
+//Boton RESET
+
+document.getElementById("r").onclick = function () {
+    document.getElementById("a").innerHTML="Inglés",document.getElementById("b").innerHTML="Trabajo"
+    ,document.getElementById("c").innerHTML="Aprendizaje",
+    document.getElementById("d").innerHTML="Cambios"
+}
+
+
+
